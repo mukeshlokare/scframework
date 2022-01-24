3fc13e81ce0ce84372d52d75b8bba4625f53f9f5
aplyamukesh <mukesh.neosoft@apalya.com>
2022-01-24 08:53:50 +0530

    Initial commit

---
+++ build/scframework.framework-iphoneos.xcarchive/Products/Library/Frameworks/scframework.framework/Headers/scframework.h
@@ -0,0 +1,18 @@
+//
+//  scframework.h
+//  scframework
+//
+//  Created by webwerks on 23/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for scframework.
+FOUNDATION_EXPORT double scframeworkVersionNumber;
+
+//! Project version string for scframework.
+FOUNDATION_EXPORT const unsigned char scframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <scframework/PublicHeader.h>
+
+
